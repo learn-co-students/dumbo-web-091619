74f025c990ed25c051f8fb975d9a86c083f4388a
Ian Hollander <ianholla@gmail.com>
2019-10-21 10:56:04 -0400

    before sessions/cookies

--- 22-rails-cookies-session/reverse_yelp/config/routes.rb
+++ 22-rails-cookies-session/reverse_yelp/config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  root to: "restaurants#index"
+  root to: "customers#index"
   resources :customer_reviews, only: [:new, :create]
   resources :customers, only: [:show, :new, :create, :index]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  # get "/restaurants", to: "restaurants#index"
-  # get "/restaurants/new", to: "restaurants#new"
-  # get "/restaurants/:id", to: "restaurants#show"
-
-  resources :restaurants #, only: [:index, :show, :new, :create, :edit, :update]
+  resources :restaurants
 end
