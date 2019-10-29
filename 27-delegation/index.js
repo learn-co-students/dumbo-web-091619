984d20ad1bcf739c54f756c3f6d7b6eb84b2a153
Eric Kim <hyeokjungkim@gmail.com>
2019-10-29 15:43:23 -0400

    adds lecture

--- 27-delegation/index.js
+++ 27-delegation/index.js
@@ -1,13 +1,21 @@
 // element.addEventListener("event_type", function(){})
 let tacoButton = document.getElementById('taco')
 let emojisUL = document.querySelector("#blobs")
+let allEmojiLis = document.querySelectorAll("ul#blobs li")
 let newEmojiForm = document.getElementById('emoji-form')
 
+allEmojiLis.forEach(function(emojiLi){
+  // emojiLi.addEventListener("click", removeHelper)
+})
 
 tacoButton.addEventListener("click", function(){
-  let emojiLI = document.createElement("li")
-  emojiLI.innerText = "🌮"
-  emojisUL.appendChild(emojiLI)
+  let emojiLi = document.createElement("li")
+  emojiLi.dataset.emojiName = "taco"
+  emojiLi.innerText = "🌮"
+  emojisUL.append(emojiLi)
+
+  // emojiLi.addEventListener("click", removeHelper)
+
 })
 
 
@@ -20,7 +28,29 @@ function submitHandler(e){
   let emojiString = emojiInput.value
   // let emojiString = e.target.emoji.value
 
-  let emojiLI = document.createElement("li")
-  emojiLI.innerText = emojiString
-  emojisUL.appendChild(emojiLI)
+  let emojiLi = document.createElement("li")
+  emojiLi.innerText = emojiString
+  emojisUL.appendChild(emojiLi)
+
+  // emojiLi.addEventListener("click", removeHelper)
 }
+
+function removeHelper(e){
+  console.log(e.target);
+  e.target.remove()
+}
+
+emojisUL.addEventListener("click", function(evt){
+  if (evt.target.tagName === "LI") {
+    console.log(evt.target.dataset.emojiName);
+    evt.target.remove()
+  }
+})
+
+// 1) Find a stable parent on the HTML
+// 2) Add event listener on that parent
+// 3) Use an if statement to see if you clicked on the children
+
+// className, classList.contains(), matches, id, tagName
+
+// 4) Add your event listener (probably on event.target)
