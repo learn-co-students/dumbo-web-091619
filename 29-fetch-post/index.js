4d1e757ec4aed004a898c5a53ccd5b5fe47a1b89
Eric Kim <hyeokjungkim@gmail.com>
2019-10-30 17:22:32 -0400

    adds lecture

--- 29-fetch-post/index.js
+++ 29-fetch-post/index.js
@@ -11,21 +11,54 @@
 document.addEventListener("DOMContentLoaded", function(){
   let button = document.querySelector("#hello-button")
   let foxesUL = document.querySelector("#foxes")
+  let newFoxForm = document.getElementById("fox-form")
+
+  newFoxForm.addEventListener("submit", (evt) => {
+    evt.preventDefault()
+
+    let newFoxName = evt.target.fox_name.value
+    let newFoxImage = evt.target.fox_img.value
+
+    // POST "http://localhost:3000/foxes"
+    fetch("http://localhost:3000/foxes", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: newFoxName,
+        img_url: newFoxImage
+      })
+    })
+    .then(r => r.json())
+    .then((fox) => {
+      turnJSONIntoLi(fox)
+    })
+
+  })
 
   button.addEventListener('click', (evt) => {
     fetch("http://localhost:3000/foxes/random")
       .then(res => res.json())
-      .then(fox => {
-        let newFoxLi = document.createElement("li")
-        let newFoxImg = document.createElement("img")
-
-        newFoxImg.src = fox.img_url
-        newFoxImg.alt = fox.name
-
-        newFoxLi.append(newFoxImg)
-        foxesUL.append(newFoxLi)
-      })
+      .then(turnJSONIntoLi)
 
   })
+
+
+  function turnJSONIntoLi(fox) {
+    let newFoxLi = document.createElement("li")
+    let newFoxImg = document.createElement("img")
+
+    newFoxImg.src = fox.img_url
+    newFoxImg.alt = fox.name
+    newFoxLi.dataset.id = fox.id
+
+    newFoxLi.append(newFoxImg)
+    foxesUL.append(newFoxLi)
+  }
+
+
+
 
 })
