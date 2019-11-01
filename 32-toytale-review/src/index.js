a007f51a94181c1f12c377e04a9f115eedae4726
othaderek <otha.hernandez@gmail.com>
2019-11-01 13:17:19 -0400

    adds updated lecture code

--- 32-toytale-review/src/index.js
+++ 32-toytale-review/src/index.js
@@ -18,13 +18,22 @@ document.addEventListener("DOMContentLoaded", ()=>{
   })
 })
 
-// Three Pillars 
+// *REMEMBER*
+// Three Pillars of JS
 // When X event happens (Event Listeners)
 // I want to do Y fetch (Getting/sending of information to the API/database)
 // And then slap Z on (off) the DOM (Changing how our website looks like)
 
-// Mise en place
+// Mise en place (Everything in it's place)
 // Find your process and stick to it (at first)
+
+// Break things down into steps
+// Those steps turn into more steps..
+// Which in turn turn into more steps!
+// Then you'll have code at some point.
+// It's not magic! It's a process. 
+// When in doubt DEBUG! (With debugger)
+
 
 
 
@@ -37,11 +46,14 @@ function getToys(){
     .then( res => res.json());
 }
 
+// The deliverables wants us to make:
 // button class (like-button)
 // p with likes
 // img tag with src (toy image), class (toy-avatar)
 // h2 tag with toys name
 // Make div class of card (setAttribute)
+// Post a new toy (we are gonna need a post fetch and an event listener for this)
+// Add likes (we are gonne need a fetch with a patch method for this, also and event listener)
 
 // Render toys
 function renderToys(toy){
@@ -67,14 +79,14 @@ function renderToys(toy){
   // append
   divCard.append(h2, img, p, button);
   toyCollection.append(divCard);
-
+  
   button.addEventListener('click', (e) => {
     like(e);
-    // debugger;
   });
   
 }
 
+// Time to POST
 function postToy(toy){
   fetch('http://localhost:3000/toys', {
     method: "POST",
@@ -94,10 +106,12 @@ function postToy(toy){
   })
 }
 
+// Time to PATCH
 function like(toy){
   let id = toy.target.previousElementSibling.id
   let addLike = parseInt(toy.target.previousElementSibling.innerText) + 1;
-  // debugger
+  // REMEMBER: When posting and patching, fetch takes 2 arguments.
+  // the URL and an object {}
   fetch(`http://localhost:3000/toys/${id}`, {
     method: "PATCH",
     headers: {
@@ -114,148 +128,14 @@ function like(toy){
   })
 }
 
-// Post Fetch
-// Second argument object
 // event listener
 
 
 
 
 
-// patch 
 
 getToys().then( toys => {
   toys.forEach(renderToys)
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Fetch
-// function getToys(){
-//   return fetch('http://localhost:3000/toys')
-//     .then( function(response){
-//       return response.json();
-//     });
-// }
-// function renderToys(toy){
-//   let toyCollection = document.querySelector('#toy-collection');
-//   // Create html elements
-//   // Set attributes
-//   // h2
-//   let h2 = document.createElement('h2');
-//   // img (setAttribute src)
-//   let img = document.createElement('img');
-//   img.setAttribute('src', toy.image);
-//   img.setAttribute('class', 'toy-avatar')
-//   // p
-//   let p = document.createElement('p');
-//   p.innerHTML = `${toy.likes} likes`
-//   // button (setAttribute class=like-btn)
-//   let button = document.createElement('button');
-//   button.setAttribute('class', 'like-button');
-//   button.setAttribute('id', toy.id);
-//   button.innerHTML = 'Like'
-// // Patch Event listener
-//   button.addEventListener('click', (e) => {
-//     like(e)
-//   });
-//   // divCard (setAttribute class=card)
-//   let divCard = document.createElement('div');
-//   divCard.setAttribute('class', 'card')
-//   // Append to dom
-//   divCard.append(h2, img, p, button);
-//   toyCollection.append(divCard);
-// }
-
-// // Post fetch
-// function postToy(e){
-//   e.preventDefault();
-//   fetch('http://localhost:3000/toys', {
-//     method: 'POST',
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify({
-//       "name": e.target.name.value,
-//       "image": e.target.image.value,
-//       "likes": 0
-//     })
-//   })
-//   .then( res => res.json())
-//   .then(renderToys);
-// }
-
-// // Patch fetch
-// function like(e){
-//   let addLike = parseInt(e.target.previousElementSibling.innerText) + 1
-//   fetch(`http://localhost:3000/toys/${e.target.id}`, {
-//     method: "PATCH",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify({
-//       likes: addLike
-//     })
-//   })
-//   .then( res => res.json())
-//   .then( toy => {
-//     e.target.previousElementSibling.innerText = `${toy.likes} likes`
-//   });
-// }
-
-
-
-// getToys().then( function(toysObject){
-//   toysObject.forEach((toy) => {
-//     renderToys(toy); 
-//   });
-// })
