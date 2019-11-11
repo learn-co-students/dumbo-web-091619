3d23b2c50d27048d678d9676dc6fa3d2a30b9782
Eric Kim <hyeokjungkim@gmail.com>
2019-11-11 11:25:00 -0500

    adds lecture

--- 37-oojs/index.js
+++ 37-oojs/index.js
@@ -1,30 +1,99 @@
-const otha = {
-  fullName: "Otha Hernandez",
-  name: "Otha",
-  introduce: function(){
-    return "I am Otha Hernandez"
+class Instructor extends Person{
+  static allArray = []
+
+  constructor(name, fullName){
+    // `this` inside of a constructor is the instance
+    super()
+    this.name = name
+    this.fullName = fullName
+    Instructor.allArray.push(this)
   }
+
+  set legal(name){
+    this.legalName = name
+  }
+
+  get allName(){
+    return `${this.name} ${this.fullName}`
+  }
+
+  introduce(){
+    // `this` is the instance
+    return `Hello, I am ${this.fullName}.`
+  }
+
+  dance(){
+    return super.dance() + "with a stranger."
+  }
+
+  static all(){
+    return "Here are all of my instructors!"
+  }
+
 }
 
-const eric = {
-  legalName: "Hyeok Jung Kim",
-  name: "Eric",
-  introduce: function(){
-    return "I am Hyeok Jung Kim"
-  }
-}
 
-const gracie = {
-  fullName: "Gracie McGuire",
-  name: "Gracie",
-  introduce: function(){
-    return "I am Gracie McGuire"
-  }
-}
+let eric = new Instructor("Eric", "Hyeok Jung Kim")
+let otha = new Instructor("Otha", "Otha Hernandez")
+let gracie = new Instructor("Gracie", "Gracie McGuire")
 
+
+
+
+
+
+
+
+//
+// function globalIntroduction(){
+//   console.log(this);
+//   return `I am ${this.fullName}!!!`
+// }
+//
+// const otha = {
+//   fullName: "Otha Hernandez",
+//   name: "Otha",
+//   introduce: globalIntroduction
+// }
+//
+// const eric = {
+//   fullName: "Hyeok Jung Kim",
+//   name: "Eric",
+//   introduce: globalIntroduction
+// }
+//
+// const gracie = {
+//   fullName: "Gracie McGuire",
+//   name: "Gracie",
+//   introduce: globalIntroduction
+// }
+
+// console.log(otha.introduce())
 // console.log(eric.introduce())
 // console.log(gracie.introduce())
-// console.log(otha.introduce())
 
-// console.log(eric.introduce === gracie.introduce)
+// console.log(eric.introduce);
+// console.log(gracie.introduce);
+
 // console.log(eric.introduce() === gracie.introduce())
+// console.log(eric.introduce === otha.introduce)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
---
+++ 37-oojs/person.js
@@ -0,0 +1,5 @@
+class Person {
+  dance(){
+    return "I am dancing!"
+  }
+}
